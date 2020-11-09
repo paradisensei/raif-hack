ba729f17d7e26cb5facf3305dc679daad78729d0
Aidar Shaifutdinov <aidarshaifutdinov@gmail.com>
2020-11-08 21:24:11 -0800

    Add bootstrap theme for dashboard UI

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='assets')
 
 @app.route('/')
 def home() -> 'html':
-    return render_template('home.html')
+    #TODO calculate average check on mocked data
+    avg_check = 100
+    return render_template('index.html', avg_check=avg_check)
 
 if __name__ == "__main__":
     app.run(debug=True)
