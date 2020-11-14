5d898ce7644eb9690b5eab50826416caa3f7196c
Aidar Shaifutdinov <aidarshaifutdinov@gmail.com>
2020-11-14 07:14:35 -0800

    Add more metrics to UI

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 import json
 import util
 from backend import metric_buttons
@@ -11,20 +11,40 @@ def get_login() -> 'html':
 
 @app.route('/login', methods=['POST'])
 def post_login():
-    # todo simple auth? search by company name
-    return redirect(url_for('dashboard'))
+    # get merchant name and pass it to dashboard
+    return redirect(url_for('dashboard', merchant=request.form['merchant']))
 
 @app.route('/dashboard')
 def dashboard() -> 'html':
-    avg_bill = metric_buttons.button_AverageBill()
+    # get merchant from param
+    merchant = request.args['merchant']
+
+    # single-valued metrics
+    avg_bill = round(metric_buttons.button_AverageBill())
+    ltv = metric_buttons.button_LTV()
+
+    # doughnut-graph metrics
     gender = metric_buttons.button_Gender()
+    age = metric_buttons.button_Age()
+
+    # value-graph metrics
     client_avg_bill = metric_buttons.button_ClientAverageBill()
+    client_avg_tx = metric_buttons.button_AverageTransactionNumber()
+
+    # rating-graph metrics
+    income_in_segment = metric_buttons.button_IncomeInSegmentRate(merchant)[1:]
+    # clients_in_segment = metric_buttons.button_ClientNumberInSegmentRate(merchant)
 
     return render_template(
         'index.html',
+        merchant=merchant,
+        avg_ltv = util.avg_ltv(ltv),
         avg_bill=avg_bill,
         gender=util.gender_graph(gender),
-        client_avg_bill=util.avg_bill_graph(client_avg_bill)
+        age=util.age_graph(age),
+        client_avg_bill=util.avg_bill_graph(client_avg_bill),
+        client_avg_tx=util.avg_tx_graph(client_avg_tx),
+        income_in_segment=util.income_in_segment(income_in_segment),
     )
 
 if __name__ == "__main__":
--- util.py
+++ util.py
@@ -4,6 +4,11 @@ import json
 def random_hex_color() -> str:
     return "#{:06x}".format(random.randint(0, 0xFFFFFF))
 
+def avg_ltv(ltv):
+    ltv_arr = list(ltv.values.reshape(-1))
+    avg_ltv = sum(ltv_arr) / len(ltv_arr)
+    return round(avg_ltv)
+
 def gender_graph(gender) -> str:
     count = gender.size
     background_color = [random_hex_color() for _ in range(count)]
@@ -28,8 +33,31 @@ def gender_graph(gender) -> str:
 
     return json.dumps(graph_data)
 
-def avg_bill_graph(avg_bill) -> str:
+def age_graph(age) -> str:
+    count = age.size
+    background_color = [random_hex_color() for _ in range(count)]
 
+    graph_data = {
+        'type': 'doughnut',
+        'data': {
+            'labels': list(age.index.values),
+            'datasets': [{
+                'label': '',
+                'data': list(age.values.reshape(-1)),
+                # 'backgroundColor': ['#4e73df', '#1cc88a', '#36b9cc'],
+                'backgroundColor': background_color,
+                'borderColor': ['#ffffff'] * count
+            }],
+        },
+        'options': {
+            'maintainAspectRatio': False,
+            'legend': {'display': False, 'title': {}}
+        }
+    }
+
+    return json.dumps(graph_data)
+
+def avg_bill_graph(avg_bill) -> str:
     graph_data = {
         'type': 'line',
         'data': {
@@ -79,5 +107,63 @@ def avg_bill_graph(avg_bill) -> str:
             }
         }
     }
-
     return json.dumps(graph_data)
+
+def avg_tx_graph(avg_tx) -> str:
+    graph_data = {
+        'type': 'line',
+        'data': {
+            'labels': list(map(lambda i: i.astype(str)[:10], list(avg_tx.index.values))),
+            'datasets': [{
+                'label': 'Txs',
+                'fill': True,
+                'data': list(avg_tx.values.reshape(-1)),
+                'backgroundColor': 'rgba(78, 115, 223, 0.05)',
+                'borderColor': 'rgba(78, 115, 223, 1)'
+            }],
+        },
+        'options': {
+            'maintainAspectRatio': False,
+            'legend': {'display': False},
+            'title': {},
+            'scales': {
+                'xAxes': [{
+                    'gridLines': {
+                        'color': 'rgb(234, 236, 244)',
+                        'zeroLineColor': 'rgb(234, 236, 244)',
+                        'drawBorder': False,
+                        'drawTicks': False,
+                        'borderDash': [2],
+                        'zeroLineBorderDash': [2],
+                        'drawOnChartArea': False
+                    },
+                    'ticks': {
+                        'fontColor': '#858796',
+                        'padding': 20
+                    }
+                }],
+                'yAxes': [{
+                    'gridLines': {
+                        'color': 'rgb(234, 236, 244)',
+                        'zeroLineColor': 'rgb(234, 236, 244)',
+                        'drawBorder': False,
+                        'drawTicks': False,
+                        'borderDash': [2],
+                        'zeroLineBorderDash': [2]
+                    },
+                    'ticks': {
+                        'fontColor': '#858796',
+                        'padding': 20
+                    }
+                }]
+            }
+        }
+    }
+    return json.dumps(graph_data)
+
+def income_in_segment(income_in_segment):
+    max_income_in_segment = max(list(map(lambda iis: iis[2], income_in_segment)))
+
+    bar_colors = ['bg-warning', 'bg-primary', 'bg-info', 'bg-success', 'bg-default']
+    income_in_segment = list(map(lambda iis: (iis[0], round((iis[2]/max_income_in_segment)*100), iis[2], bar_colors[iis[1]-1]), income_in_segment))
+    return income_in_segment
